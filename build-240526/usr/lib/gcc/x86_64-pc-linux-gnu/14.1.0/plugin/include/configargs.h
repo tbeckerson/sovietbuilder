e3a1142e42acdcf165b01ce7612cc46cb92ec1e0
Travis Bickmann <bickmann.travis@gmail.com>
2024-06-09 11:38:42 -0300

    fixes

--- build-240526/usr/lib/gcc/x86_64-pc-linux-gnu/14.1.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../configure --prefix=/usr --enable-lto --enable-shared --enable-languages=c,c++ --enable-default-pie --enable-default-ssp --disable-multilib --disable-fixincludes --disable-bootstrap --with-system-zlib";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
